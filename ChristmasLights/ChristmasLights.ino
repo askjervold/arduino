32b04548e8602d4728726766e9c21d5d42d26f66
Audun Skjervold <audun@skjervold.me>
2014-12-12 23:44:52 +0100

    Default mode functional

--- ChristmasLights/ChristmasLights.ino
+++ ChristmasLights/ChristmasLights.ino
@@ -1,10 +1,8 @@
 // Light pins
-const byte red_one = 3;
-const byte red_two = 5;
-const byte red_three = 7;
-const byte red_four = 9;
-const byte red_five = 11;
-const byte red_six = 13;
+const byte red_one = 5;
+const byte red_two = 7;
+const byte red_three = 9;
+const byte red_four = 11;
 const byte green_one = 4;
 const byte green_two = 6;
 const byte green_three = 8;
@@ -14,34 +12,67 @@ const byte green_five = 12;
 // Other pins
 const byte buttonPin = 2;
 
+// Other constants
+const int numLeds = 9;
+const byte leds [numLeds] = {green_one, red_one, green_two, red_two, green_three, red_three, green_four, red_four, green_five};
+
 // Variables that will change
 int playingJingle = 0;
+int inactiveLedCounter = 0;
 
 void setup() {
   // Outputs
   pinMode(green_five, OUTPUT);
+  pinMode(green_four, OUTPUT);
+  pinMode(green_three, OUTPUT);
+  pinMode(green_two, OUTPUT);
+  pinMode(green_one, OUTPUT);
+  pinMode(red_four, OUTPUT);
+  pinMode(red_three, OUTPUT);
+  pinMode(red_two, OUTPUT);
+  pinMode(red_one, OUTPUT);
   
   // Inputs
   pinMode(buttonPin, INPUT);
+  
+  // Starting leds
+  greensOn();
+  redsOn();
 }
 
 void loop() {
-  // When button is pressed ...
-  if (digitalRead(buttonPin) == HIGH) {
-    if (!playingJingle) {
+  if (!playingJingle) {
+    if (digitalRead(buttonPin) == HIGH) {
       playJingle();
-      redsOn();
-      greensOff();  
     }
-    else {
-      playJingle();
-      redsOff();
-      greensOn();
-    }
+    loopLeds();
+    delay(100);
+  }
+}
+
+void loopLeds() {
+  int turnOn = inactiveLedCounter - 1;
+  if (turnOn != -1) {
+    digitalWrite(leds[turnOn], HIGH);
+  }
+  else {
+    digitalWrite(leds[numLeds], HIGH);
+  }
+  digitalWrite(leds[inactiveLedCounter], LOW);
+  if (inactiveLedCounter++ == numLeds) {
+    inactiveLedCounter = 0;
   }
 }
 
 void playJingle() {
+  if (!playingJingle) {
+      redsOn();
+      greensOff();  
+    }
+    else {
+      redsOff();
+      greensOn();
+    }
   playingJingle = !playingJingle;
   delay(250);
 }
@@ -67,8 +98,6 @@ void redsOn() {
   digitalWrite(red_two, HIGH);
   digitalWrite(red_three, HIGH);
   digitalWrite(red_four, HIGH);
-  digitalWrite(red_five, HIGH);
-  digitalWrite(red_six, HIGH);
 }
 
 void redsOff() {
@@ -76,6 +105,4 @@ void redsOff() {
   digitalWrite(red_two, LOW);
   digitalWrite(red_three, LOW);
   digitalWrite(red_four, LOW);
-  digitalWrite(red_five, LOW);
-  digitalWrite(red_six, LOW);
 }
